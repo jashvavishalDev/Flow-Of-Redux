26bb3438b5f30a4e446f259c05938cbd669da006
jashvavishalDev <informjashvavishal@gmail.com>
2022-06-16 13:22:11 +0530

    Using Redux Toolkit -1.  npm install @reduxjs/toolkit -2. import { configureStore, createSlice } from "@reduxjs/toolkit"; -3. const ---= createSlice({ name: " ", initialState: { }, reducers: { } }) export const actions = ---.actions;  const store =configureStore({ reducer: ----.reducer, })

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+//using redux toolkit
+
 import {useDispatch, useSelector} from 'react-redux';
+import {actions} from './store/index';
 
 function App() {
 
   const counter= useSelector(( state )=>  state.counter  );
   const dispatch= useDispatch();
 
-  const onIncrement=()=>{ dispatch( { type: "INC" } ); };
+  const onIncrement=()=>{ dispatch(actions.onIncrement() ); };
 
-  const onDecrement=()=>{ dispatch( { type: "DEC" } ); };
+  const onDecrement=()=>{ dispatch( actions.onDecrement() ); };
 
-  const addBy =() =>{ dispatch( { type: "ADD", payload: 10} ); };
+  const addBy =() =>{ dispatch( actions.addBy(10) ); };
 
   return (
     <div>
@@ -23,3 +26,33 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+// import {useDispatch, useSelector} from 'react-redux';
+
+// function App() {
+
+//   const counter= useSelector(( state )=>  state.counter  );
+//   const dispatch= useDispatch();
+
+//   const onIncrement=()=>{ dispatch( { type: "INC" } ); };
+
+//   const onDecrement=()=>{ dispatch( { type: "DEC" } ); };
+
+//   const addBy =() =>{ dispatch( { type: "ADD", payload: 10} ); };
+
+//   return (
+//     <div>
+//        <h1> Counter App </h1>
+//        <h2> {counter} </h2>
+//        <button onClick={onIncrement}>Increment</button>
+//        <button onClick={onDecrement}>Decrement</button>
+//        <button onClick={addBy}>Add by 10</button>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,51 @@
-import {createStore} from "redux";
+// using Redux ToolKit
+import { configureStore, createSlice} from "@reduxjs/toolkit";
 
+const counterSlice= createSlice({
+    name: "counter",
+    initialState: {counter: 0},
+    reducers: {
+        onIncrement(state, action){
+            state.counter++;
+        },
+        onDecrement(state, action){
+            state.counter--;
+        },
+        addBy(state, action){
+            state.counter += action.payload;
+        },
+    },
+})
 
-const reducerFn= (state= { counter: 0 }, action) => {
-    // Synchronous Function
-    //We should not  mutate the original state
-    if (action.type === "INC"){
-        return{ counter: state.counter + 1 };
-    }
-    if (action.type === "DEC"){
-        return{ counter: state.counter - 1 };
-    }
-    if (action.type === "ADD"){
-        return{ counter: state.counter + action.payload };
-    }
-    return state;
-  
-};
-
-const store= createStore(reducerFn);
+export const actions= counterSlice.actions;
+const store =configureStore({
+    reducer: counterSlice.reducer,
+})
 
 export default store;
+
+
+
+
+// import {createStore} from "redux";
+
+
+// const reducerFn= (state= { counter: 0 }, action) => {
+//     // Synchronous Function
+//     //We should not  mutate the original state
+//     if (action.type === "INC"){
+//         return{ counter: state.counter + 1 };
+//     }
+//     if (action.type === "DEC"){
+//         return{ counter: state.counter - 1 };
+//     }
+//     if (action.type === "ADD"){
+//         return{ counter: state.counter + action.payload };
+//     }
+//     return state;
+  
+// };
+
+// const store= createStore(reducerFn);
+
+// export default store;
